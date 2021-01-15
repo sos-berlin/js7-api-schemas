44648e91349e29942a498348b55947d7e933d81e
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2021-01-15 15:50:46 +0100

    deleteDraft exanple

--- src/main/resources/com/sos/joc/model/common/IConfigurationObject.java
+++ src/main/resources/com/sos/joc/model/common/IConfigurationObject.java
@@ -2,9 +2,6 @@ package com.sos.joc.model.common;
 
 public interface IConfigurationObject {
     
-    public String getPath();
-    public void setPath(String path);
-    
     public String getTitle();
     public void setTitle(String path);
     
