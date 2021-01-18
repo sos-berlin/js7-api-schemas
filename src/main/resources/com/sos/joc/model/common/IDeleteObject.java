46762c4ac26a2f0f07d31b7c95de136cb5abf606
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-01-18 12:21:50 +0100

    IDeleteObject, IDeployObject - versionId, path removed

--- src/main/resources/com/sos/joc/model/common/IDeleteObject.java
+++ src/main/resources/com/sos/joc/model/common/IDeleteObject.java
@@ -2,6 +2,5 @@ package com.sos.joc.model.common;
 
 public interface IDeleteObject {
     
-    public String getPath();
-    public void setPath(String path);
+    
 }
--- src/main/resources/com/sos/joc/model/common/IDeployObject.java
+++ src/main/resources/com/sos/joc/model/common/IDeployObject.java
@@ -1,7 +1,5 @@
 package com.sos.joc.model.common;
 
 public interface IDeployObject {
-    
-    public String getVersionId();
-    public void setVersionId(String path);
+
 }
